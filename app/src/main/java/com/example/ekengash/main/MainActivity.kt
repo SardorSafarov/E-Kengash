cf7956ae8da78a712132d1d8a1747c207e0ecfaf
SardorSafarov <86124288+SardorSafarov@users.noreply.github.com>
2022-02-13 13:29:35 +0500

    chap menu ishladi

--- app/src/main/java/com/example/ekengash/main/MainActivity.kt
+++ app/src/main/java/com/example/ekengash/main/MainActivity.kt
@@ -8,14 +8,13 @@ import android.graphics.Color
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.util.Log.d
-import android.widget.ImageButton
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AlertDialog
-import androidx.core.view.GravityCompat
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -26,17 +25,120 @@ import com.example.ekengash.databinding.ActivityMainBinding
 import com.example.ekengash.fragmentlar.asosiyy.Asosiy
 import com.example.ekengash.fragmentlar.chat.ChatScreen
 import com.example.ekengash.fragmentlar.kuproq.Kuproqq
-import com.example.log.D
+
 
 class MainActivity : AppCompatActivity() {
+
+
     lateinit var binding: ActivityMainBinding
+
+
+
+
     @SuppressLint("WrongViewCast")
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        window.statusBarColor=Color.WHITE
+        teginma()
+
+    }
+
+
+
+
+
+
+    private fun bottomBarSetOnclickListener() {
+        supportFragmentManager.beginTransaction().replace(
+            R.id.nav_host_fragment_activity_main,
+            Asosiy()
+        ).commit()
+        binding.navView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.asosiy -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.nav_host_fragment_activity_main, Asosiy()).commit()
+                    true
+                }
+                R.id.kompas -> {
+//                    supportFragmentManager.beginTransaction().replace(R.id.nav_host_fragment_activity_main,
+//                        Murojatlar()
+//                    ).commit()
+                    true
+                }
+                R.id.chat -> {
+                    startActivity(Intent(this, ChatScreen::class.java))
+                    true
+                }
+                R.id.kuproq -> {
+                    supportFragmentManager.beginTransaction().replace(
+                        R.id.nav_host_fragment_activity_main,
+                        Kuproqq()
+                    ).commit()
+                    true
+                }
+                else -> true
+            }
+        }
+    }
+
+
+    ///-------------------Tegma-----------------------------/////
+
+    private var drawerLayout: DrawerLayout? = null
+    private var toggle: ActionBarDrawerToggle? = null
+    private var toolbar: Toolbar? = null
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun teginma() {
+        iternetniTekshirish()
+        bottomBarSetOnclickListener()
+        blok()
+        statusBar()
+        chapMenu()
+    }
+
+    private fun chapMenu() {
+        drawerLayout = binding.drawerLayout
+        toolbar = binding.toolbarMain
+        setSupportActionBar(toolbar)
+        supportActionBar!!.title = ""
+        toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar,
+            R.string.app_name, androidx.appcompat.R.string.abc_menu_alt_shortcut_label)
+        drawerLayout!!.addDrawerListener(toggle!!)
+        toggle!!.syncState()
+    }
+
+    private val blok: Blok by lazy {
+        ViewModelProviders.of(this).get(Blok::class.java)
+    }
+
+    private fun blok() {
+        checkMone()
+    }
+
+    private fun checkMone() {
+        blok.readLocation()
+        blok.blok.observe(this, Observer {
+            try {
+                if (it[0].blok) {
+                } else {
+                    var intent = Intent(this, BlokActivitt::class.java)
+                    intent.putExtra("xabar", it[0].text)
+                    startActivity(intent)
+                    finish()
+
+                }
+            } catch (e: Exception) {
+
+            }
+        })
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun iternetniTekshirish() {
         if (isOnline(context = this)) {
 
         } else {
@@ -46,91 +148,13 @@ class MainActivity : AppCompatActivity() {
             }
             alerDialogBinding.show()
         }
-        bottomBarSetOnclickListener()
-        blok()
-    }
-
-    fun openCloseNavigationDrawer() {
-        try {
-            val drawer = (R.id.asosiy_menu_chap) as DrawerLayout
-            if (drawer.isDrawerOpen(GravityCompat.START)) {
-                drawer.closeDrawer(GravityCompat.START)
-            } else {
-                drawer.openDrawer(GravityCompat.START)
-            }
-        }catch (e:Exception){
-            e.message?.let { D.d(it) }
-        }
-
-    }
-    private fun bottomBarSetOnclickListener() {
-        supportFragmentManager.beginTransaction().replace(
-            R.id.nav_host_fragment_activity_main,
-            Asosiy()
-        ).commit()
-        binding.navView.setOnItemSelectedListener {
-            when(it.itemId)
-            {
-                R.id.asosiy ->{
-                    supportFragmentManager.beginTransaction().replace(R.id.nav_host_fragment_activity_main,Asosiy()).commit()
-                    true
-                }
-                R.id.kompas ->
-                {
-//                    supportFragmentManager.beginTransaction().replace(R.id.nav_host_fragment_activity_main,
-//                        Murojatlar()
-//                    ).commit()
-                    true
-                }
-                R.id.chat ->{
-                   startActivity(Intent(this,ChatScreen::class.java))
-                    true
-                }
-                R.id.kuproq ->{
-                    supportFragmentManager.beginTransaction().replace(
-                        R.id.nav_host_fragment_activity_main,
-                        Kuproqq()
-                    ).commit()
-                    true
-                }
-                else->true
-            }
-        }
     }
 
 
 
-    ///-----------tegma-----------------/////
-
-    private val blok: Blok by lazy {
-        ViewModelProviders.of(this).get(Blok::class.java)
+    private fun statusBar() {
+        window.statusBarColor = Color.WHITE
     }
-
-    private fun blok() {
-   // blok.insertLocation(BlokEntity())
-        checkMone()
-    }
-
-    private fun checkMone() {
-        blok.readLocation()
-        blok.blok.observe(this, Observer {
-            try {
-              if(it[0].blok)
-              {
-              }else{
-                  var intent=Intent(this,BlokActivitt::class.java)
-                  intent.putExtra("xabar",it[0].text)
-                  startActivity(intent)
-                  finish()
-
-              }
-            }catch (e:Exception)
-            {
-
-            }
-        })
-    }
-
 
     @RequiresApi(Build.VERSION_CODES.M)
     fun isOnline(context: Context): Boolean {
