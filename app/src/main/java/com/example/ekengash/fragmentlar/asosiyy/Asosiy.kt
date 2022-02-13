586bd262b618d1943e443eeea06403d9b6628d0d
SardorSafarov <86124288+SardorSafarov@users.noreply.github.com>
2022-02-13 17:48:21 +0500

    asosiy serves buttonlar ishladi

--- app/src/main/java/com/example/ekengash/fragmentlar/asosiyy/Asosiy.kt
+++ app/src/main/java/com/example/ekengash/fragmentlar/asosiyy/Asosiy.kt
@@ -14,6 +14,9 @@ import com.example.ekengash.databinding.FragmentAsosiy2Binding
 import com.example.ekengash.entity.AsosiyServislarButtonEntity
 import com.example.ekengash.entity.KuproqItemEntitit
 import com.example.ekengash.fragmentlar.asosiyy.asosiy_qidirish_oynasi.AsosiyQidirish
+import com.example.ekengash.servislar.aviachipta.Aviachipta
+import com.example.ekengash.servislar.avtobus.Avtobus
+import com.example.ekengash.servislar.poyezd.Poyezd
 import com.example.log.D
 
 
@@ -22,7 +25,7 @@ class Asosiy : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         asosiyQidirishOynasi()
-
+        servesButton()
     }
 
 
@@ -30,43 +33,71 @@ class Asosiy : Fragment() {
 
     /*--------------------------------------Teginma-------------------------------------------------------------*/
 
-    private val list:List<AsosiyServislarButtonEntity> = listOf(
-        AsosiyServislarButtonEntity(
-            buttonName = listOf(
-                KuproqItemEntitit(
-                    icon = R.drawable.ic_a_b,
-                    text = "A_B"
-                ),
-                KuproqItemEntitit(
-                    icon = R.drawable.ic_avya,
-                    text = "Aviabelet"
-                ),
-                KuproqItemEntitit(
-                    icon = R.drawable.ic_poyiz,
-                    text = "Poyezd"
-                ),
-                KuproqItemEntitit(
-                    icon = R.drawable.ic_mehmonhonalar,
-                    text = "Turar-joy"
-                ),
-                KuproqItemEntitit(
-                    icon = R.drawable.ic_avtobus,
-                    text = "Avtobus"
-                ),
-                KuproqItemEntitit(
-                    icon = R.drawable.ic_taxi,
-                    text = "Taksi"
-                ),
-                KuproqItemEntitit(
-                    icon = R.drawable.ic_turpaket,
-                    text = "Turpaket"
-                ),
-                KuproqItemEntitit(
-                    icon = R.drawable.ic_chegirmalar,
-                    text = "Chegirmalar"
-                )
-            )
-        ),
+    private fun servesButton() {
+        binding.servesAviabeletButton.setOnClickListener {
+            startActivity(Intent(context,Aviachipta::class.java))
+        }
+        binding.servesAvtobusButton.setOnClickListener {
+            startActivity(Intent(context,Avtobus::class.java))
+        }
+        binding.servesPoyezdButton.setOnClickListener {
+            startActivity(Intent(context,Poyezd::class.java))
+        }
+    }
+    private fun asosiyQidirishOynasi() {
+        binding.asosiyQidirishButton.setOnClickListener {
+            startActivity(Intent(context,AsosiyQidirish::class.java))
+        }
+    }
+    private var _binding: FragmentAsosiy2Binding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentAsosiy2Binding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+    //    private val list:List<AsosiyServislarButtonEntity> = listOf(
+//        AsosiyServislarButtonEntity(
+//            buttonName = listOf(
+//                KuproqItemEntitit(
+//                    icon = R.drawable.ic_a_b,
+//                    text = "A_B"
+//                ),
+//                KuproqItemEntitit(
+//                    icon = R.drawable.ic_avya,
+//                    text = "Aviabelet"
+//                ),
+//                KuproqItemEntitit(
+//                    icon = R.drawable.ic_poyiz,
+//                    text = "Poyezd"
+//                ),
+//                KuproqItemEntitit(
+//                    icon = R.drawable.ic_mehmonhonalar,
+//                    text = "Turar-joy"
+//                ),
+//                KuproqItemEntitit(
+//                    icon = R.drawable.ic_avtobus,
+//                    text = "Avtobus"
+//                ),
+//                KuproqItemEntitit(
+//                    icon = R.drawable.ic_taxi,
+//                    text = "Taksi"
+//                ),
+//                KuproqItemEntitit(
+//                    icon = R.drawable.ic_turpaket,
+//                    text = "Turpaket"
+//                ),
+//                KuproqItemEntitit(
+//                    icon = R.drawable.ic_chegirmalar,
+//                    text = "Chegirmalar"
+//                )
+//            )
+//        ),
 //        AsosiyServislarButtonEntity(
 //            buttonName = listOf(
 //                KuproqItemEntitit(
@@ -95,23 +126,6 @@ class Asosiy : Fragment() {
 //                )
 //            )
 //        )
-
-    )
-    private fun asosiyQidirishOynasi() {
-        binding.asosiyQidirishButton.setOnClickListener {
-            startActivity(Intent(context,AsosiyQidirish::class.java))
-        }
-    }
-    private var _binding: FragmentAsosiy2Binding? = null
-    private val binding get() = _binding!!
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        _binding = FragmentAsosiy2Binding.inflate(inflater, container, false)
-        val view = binding.root
-        return view
-    }
+//
+//    )
 }
