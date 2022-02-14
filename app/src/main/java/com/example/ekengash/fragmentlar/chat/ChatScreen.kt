0b33f43998f17113da97602e50ba15f5ccce52b4
SardorSafarov <86124288+SardorSafarov@users.noreply.github.com>
2022-02-14 12:03:58 +0500

    chat qismi xabar buttoni ishladi

--- app/src/main/java/com/example/ekengash/fragmentlar/chat/ChatScreen.kt
+++ app/src/main/java/com/example/ekengash/fragmentlar/chat/ChatScreen.kt
@@ -3,8 +3,13 @@ package com.example.ekengash.fragmentlar.chat
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log.d
+import androidx.core.widget.addTextChangedListener
 import com.example.ekengash.R
 import com.example.ekengash.databinding.ActivityChatScreenBinding
+import com.example.log.D
 
 class ChatScreen : AppCompatActivity() {
     lateinit var binding :ActivityChatScreenBinding
@@ -16,8 +21,32 @@ class ChatScreen : AppCompatActivity() {
         window.statusBarColor= Color.WHITE
         chatdanChiqish()
         topButton1()
-
+        xabarniKuzatish()
     }
+
+    private fun xabarniKuzatish() {
+        binding.matinliyXabar.addTextChangedListener (object :TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+              when(p0.toString().length)
+              {
+                  0->{
+                      D.d(p0.toString())
+                      binding.xabarJunatish.setImageResource(R.drawable.ic_chat_mikrafon)
+                  }
+                  else->{
+                      D.d(p0.toString())
+                      binding.xabarJunatish.setImageResource(R.drawable.ic_xabar_yuborish)
+                  }
+              }
+            }
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+        })
+    }
+
 
     private fun topButton1() {
         binding.ovozliChatButton.setOnClickListener {
