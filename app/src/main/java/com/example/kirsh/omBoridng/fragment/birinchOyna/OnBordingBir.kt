31392bd503280e3bb13c452cbfaf5842623a0ab5
SardorSafarov <86124288+SardorSafarov@users.noreply.github.com>
2022-03-26 04:58:04 +0500

    tilni boshladim ishlaydigan kod bor

--- app/src/main/java/com/example/kirsh/omBoridng/fragment/birinchOyna/OnBordingBir.kt
+++ app/src/main/java/com/example/kirsh/omBoridng/fragment/birinchOyna/OnBordingBir.kt
@@ -1,15 +1,24 @@
 package com.example.kirsh.omBoridng.fragment.birinchOyna
 
 import android.content.Context
+import android.content.SharedPreferences
+import android.content.res.Configuration
+import android.content.res.Resources
+import android.os.Build
 import android.os.Bundle
+import android.util.DisplayMetrics
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.findNavController
+import com.example.constants.Constants.SHAREDPREF_KEY
 import com.example.constants.Constants.TIL
 import com.example.katrip.R
 import com.example.katrip.databinding.FragmentOnBordingBirBinding
+import com.example.log.D
+import java.util.*
 
 
 class OnBordingBir : Fragment() {
@@ -18,6 +27,8 @@ class OnBordingBir : Fragment() {
     lateinit var title: Array<String>
     lateinit var sub_title: Array<String>
     lateinit var button:Array<String>
+    lateinit var sharedPreferences: SharedPreferences
+    lateinit var lan: String
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -29,6 +40,10 @@ class OnBordingBir : Fragment() {
         binding.davomEtishButton.setOnClickListener {
             findNavController().navigate(R.id.action_onBordingBir_to_ikkiOnBording)
         }
+        sharedPreferences = requireActivity().getSharedPreferences(SHAREDPREF_KEY, AppCompatActivity.MODE_PRIVATE)
+        lan = sharedPreferences.getString("key", null).toString()
+        D.d(lan)
+        setApplicationLocale("ru")
         return view
     }
 
@@ -36,26 +51,37 @@ class OnBordingBir : Fragment() {
         super.onActivityCreated(savedInstanceState)
         til()
     }
+    private fun setApplicationLocale(locale: String) {
+        val resources: Resources = resources
+        val dm: DisplayMetrics = resources.getDisplayMetrics()
+        val config: Configuration = resources.getConfiguration()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            config.setLocale(Locale(locale.toLowerCase()))
+        } else {
+            config.locale = Locale(locale.toLowerCase())
+        }
+        resources.updateConfiguration(config, dm)
+    }
 
     private fun til() {
-        when (TIL) {
-            "uz" -> {
-                binding.title.setText(title[0])
-                binding.subTitle.setText(sub_title[0])
-                binding.btnText.setText(button[0])
-            }
-            "ru" -> {
-                binding.title.setText(title[3])
-                binding.subTitle.setText(sub_title[3])
-                binding.btnText.setText(button[1])
-
-            }
-            "en" -> {
-                binding.title.setText(title[6])
-                binding.subTitle.setText(sub_title[6])
-                binding.btnText.setText(button[2])
-            }
-        }
+//        when (TIL) {
+//            "uz" -> {
+//                binding.title.setText(title[0])
+//                binding.subTitle.setText(sub_title[0])
+//                binding.btnText.setText(button[0])
+//            }
+//            "ru" -> {
+//                binding.title.setText(title[3])
+//                binding.subTitle.setText(sub_title[3])
+//                binding.btnText.setText(button[1])
+//
+//            }
+//            "en" -> {
+//                binding.title.setText(title[6])
+//                binding.subTitle.setText(sub_title[6])
+//                binding.btnText.setText(button[2])
+//            }
+//        }
     }
 
     override fun onAttach(context: Context) {
--- app/src/main/java/com/example/kirsh/omBoridng/fragment/tilniTanlash/TilniTanlash.kt
+++ app/src/main/java/com/example/kirsh/omBoridng/fragment/tilniTanlash/TilniTanlash.kt
@@ -1,11 +1,14 @@
 package com.example.kirsh.omBoridng.fragment.tilniTanlash
 
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.example.constants.Constants.SHAREDPREF_KEY
 import com.example.constants.Constants.TIL
 import com.example.katrip.R
 import com.example.katrip.databinding.FragmentTilniTanlashBinding
@@ -14,8 +17,7 @@ import com.example.katrip.databinding.FragmentTilniTanlashBinding
 class TilniTanlash : Fragment() {
     private var _binding: FragmentTilniTanlashBinding? = null
     private val binding get() = _binding!!
-
-
+    lateinit var sharedPreferences: SharedPreferences
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -23,21 +25,35 @@ class TilniTanlash : Fragment() {
     ): View? {
         _binding = FragmentTilniTanlashBinding.inflate(inflater, container, false)
         val view = binding.root
+        setOnClickListener()
+        sharedPreferences = activity?.getSharedPreferences(SHAREDPREF_KEY, MODE_PRIVATE)!!
         activity?.window?.statusBarColor = resources.getColor(R.color.asosiy_orqa_ranggi)
-       binding.tilUz.setOnClickListener{
-           findNavController().navigate(R.id.action_tilniTanlash_to_onBordingBir)
+
+        return view
+    }
+
+    private fun setOnClickListener() {
+        binding.tilUz.setOnClickListener{
+            findNavController().navigate(R.id.action_tilniTanlash_to_onBordingBir)
+            sharedPreferences.edit().apply{
+            putString("til","uz")
+            }.apply()
             TIL="uz"
-       }
+        }
         binding.tilRu.setOnClickListener{
             findNavController().navigate(R.id.action_tilniTanlash_to_onBordingBir)
             TIL="ru"
-
+            sharedPreferences.edit().apply{
+                putString("til","ru")
+            }.apply()
         }
         binding.tilEn.setOnClickListener{
             findNavController().navigate(R.id.action_tilniTanlash_to_onBordingBir)
             TIL="en"
+            sharedPreferences.edit().apply{
+                putString("til","en")
+            }.apply()
         }
-        return view
     }
 
 }
